202ad91dd9da514561d49197f05ab434dff372c5
transfer76 <shina76@ukr.net>
2019-04-20 00:58:04 +0300

    Set room channel

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,15 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    logger.info "Subscribed to RoomChannel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    logger.info "Unsubscribed to RoomChannel"
   end
 
   def speak
+    logger.info "RoomChannel, speak: #{data.inspect}"
   end
 end
